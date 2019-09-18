afae4612c631c89ec6978ae131c5f43421fc7916
Megan <mennis0145@gmail.com>
2019-09-18 12:48:08 +0100

    TeamMembersList initialised to default list of team members, rendering each member to screen

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import uuid from "uuid";
 import './App.css';
 
+const initialTeamMembersList = [
+  {id: uuid(), name: 'Mark', age: '21', position: 'SL'},
+  {id: uuid(), name: 'Tony', age: '29', position: 'TL'},
+  {id: uuid(), name: 'Sarah', age: '25', position: 'Student'},
+]
+
 function App() {
-  const [teamMembers, setTeamMembers] = useState();
+  const [teamMembersList, setTeamMembersList] = useState(initialTeamMembersList);
 
   return (
     <div className="App">
-
+      {
+        teamMembersList.map(teamMember => (
+          <h5 key={teamMember.id}>
+          {teamMember.name}
+          </h5>
+        ))
+      }
     </div>
   );
 }
