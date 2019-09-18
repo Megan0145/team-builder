44c2c34cb0563587657bf241789c4585e682f9ef
Megan <mennis0145@gmail.com>
2019-09-18 12:39:52 +0100

    imported useState hook and set state property for team members list

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import uuid from "uuid";
+import './App.css';
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState();
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
